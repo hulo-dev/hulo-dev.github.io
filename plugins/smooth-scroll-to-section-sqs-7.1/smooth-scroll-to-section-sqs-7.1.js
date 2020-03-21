b8e29e6e636669d94156d7c29f1091756c9f41e3
hulo-dev <59173765+hulo-dev@users.noreply.github.com>
2020-03-21 21:50:46 +0200

    Create  smooth-scroll-to-section-sqs-7.1.js

---
+++ plugins/smooth-scroll-to-section-sqs-7.1/smooth-scroll-to-section-sqs-7.1.js
@@ -0,0 +1,16 @@
+  document.querySelectorAll('[data-section-anchor]').forEach(function(sectionAnchor) {
+    var parentSection =  sectionAnchor.closest("section");
+    sectionAnchor.closest(".sqs-block-code").classList.add('code-block-with-anchor');
+    parentSection.setAttribute('id', sectionAnchor.getAttribute('data-section-anchor'));
+    parentSection.setAttribute('data-scroll-offset', sectionAnchor.getAttribute('data-offset'));
+  });
+
+  document.querySelectorAll('a[href*="-anchor"]').forEach(function(anchorLink) {
+    anchorLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      var sectionId = anchorLink.getAttribute('href').replace(/^#/, "");
+      var scrollOffset = document.querySelectorAll('[data-section-anchor="'+sectionId+'"]')[0].getAttribute('data-offset') || 0;
+      EPPZScrollTo.scrollVerticalToElementById(sectionId, scrollOffset);
+    })
+  });
