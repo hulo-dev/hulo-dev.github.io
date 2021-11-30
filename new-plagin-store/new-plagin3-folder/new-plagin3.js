f964125cb3a725848a3f936c9da487b5c713f002
hulo.dev <59173765+hulo-dev@users.noreply.github.com>
2021-11-30 13:35:03 +0200

    Update new-plagin3.js

--- new-plagin-store/new-plagin3-folder/new-plagin3.js
+++ new-plagin-store/new-plagin3-folder/new-plagin3.js
@@ -2,7 +2,7 @@
 //main-banner-slider
   import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
 
-const swipers = new Swiper(".main-banner-slider", {
+const swipers = Swiper(".main-banner-slider", {
     fadeEffect: { crossFade: true },
     autoplay: {
         delay: 2500,
