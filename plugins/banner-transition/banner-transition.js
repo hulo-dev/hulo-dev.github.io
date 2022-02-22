530ea3cf4c08391afbdf139a53d681e3e3082a5b
hulo.dev <59173765+hulo-dev@users.noreply.github.com>
2022-02-22 11:01:33 +0200

    Create banner-transition.js

---
+++ plugins/banner-transition/banner-transition.js
@@ -0,0 +1,43 @@
+<!-- Vertical Transition Plugin -->
+
+if (window.pageBannerTransition && window.pageBannerTransition.excluded && Array.isArray(window.pageBannerTransition.excluded) && window.pageBannerTransition.excluded.length > 0)
+for (var i = 0; i < window.pageBannerTransition.excluded.length; i++) window.location.pathname.indexOf(window.pageBannerTransition.excluded[i]) > -1 && document.body.classList.add("excluded-page");
+
+if (!document.body.classList.contains("excluded-page")) { 
+
+  document.body.insertAdjacentHTML("afterbegin", '<div class="transition-slide"></div>');
+
+  var tl_intro = gsap.timeline(),
+  transitionSlide = document.querySelector(".transition-slide"),
+  site = document.getElementById("siteWrapper") || document.querySelector(".Site");
+
+  (transitionSlide.style.visibility = "visible"),
+
+  tl_intro 
+  .fromTo(transitionSlide, 1.2, { y: "100%" }, { y: "0%", ease: "expo.inOut" }, 0)   
+  .to(transitionSlide,1, { y: "-100%", ease: "expo.inOut", onComplete: function () {transitionSlide.style.visibility = "hidden";}, }, 1)
+
+  .to(site,
+    { 
+      keyframes: [
+        { y: "100vh", ease: "expo.inOut", duration: 0 },
+        { y: "20h", ease: "expo.inOut", duration: 0, delay: 1 },
+        {
+          y: "0",
+          ease: "expo.inOut",
+          duration: 1,
+          clearProps: "transform",
+          onStart: function () {
+            window.scrollTo(0, 0), (site.style.opacity = "1");
+          },
+        },
+      ],
+      onComplete: function () {
+        document.body.classList.add("transition-slide-end");
+      },
+    },        
+    0
+  );
+}
+
+<!-- Vertical Transition Plugin -->
